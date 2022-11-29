af711be25938b64549b1b78af5d9f4388fedf0a0
andynewen <117054251+andynewen@users.noreply.github.com>
2022-11-29 19:55:12 +1100

    added a function to shuffle characters

--- script.js
+++ script.js
@@ -13,16 +13,28 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//Function to shuffle all the characters types
+function CharShuffle(min, max) {
+  if (!max) {
+      max = min
+      min = 0
+  }
+  var numberValue = Math.random()
+  return Math.floor(min * (1 - numberValue) + numberValue * max)
+}
+
 
 // Windows prompt for the length of the password.
 function generatePassword(){
 
+// Variable settings the array of data by all characters
 var specialCharacters = [ '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.' ];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCasedCharacters = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
 var upperCasedCharacters = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ];
 
-var passwordlength = prompt("Please enter the length of your password.\n A password must contain atleast 8 character and no more than 128 characters .\n Pasword length must be equal to or inbetween 8 and 128 ") 
+var passwordlength = prompt("Please enter the length of your password.\nA password must contain atleast 8 character and no more than 128 characters.\nPassword length should be between 8 and 128 characters ") 
 
 //Valid password length
+
 }
