ee4a2365ded8148122023b0571fc01497d96c2e5
andynewen <117054251+andynewen@users.noreply.github.com>
2022-11-29 18:54:45 +1100

    added character code block and window prompt

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Windows prompt for the length of the password.
+function generatePassword(){
+
+var specialCharacters = [ '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.' ];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCasedCharacters = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
+var upperCasedCharacters = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ];
+
+var passwordlength = prompt("Please enter the length of your password.\n A password must contain atleast 8 character and no more than 128 characters .\n Pasword length must be equal to or inbetween 8 and 128 ") 
+
+//Valid password length
+}
