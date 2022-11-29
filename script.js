7e2efba7c2bac2b2c9284ae663bda802c065e194
andynewen <117054251+andynewen@users.noreply.github.com>
2022-11-29 21:01:43 +1100

    added var character and pw for later

--- script.js
+++ script.js
@@ -28,10 +28,14 @@ function CharShuffle(min, max) {
 function generatePassword(){
 
 // Variable settings the array of data by all characters
-var specialCharacters = [ '@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.' ];
+var specialCharacters = [ '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.' ];
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCasedCharacters = [ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' ];
-var upperCasedCharacters = [ 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' ];
+var lowerCasedCharacters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+var upperCasedCharacters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ];
+
+// Added pw and characters variable for later
+var pwCharacter = [];
+var Characters = [];
 
 var passwordlength = prompt("Please enter the length of your password.\nA password must contain atleast 8 character and no more than 128 characters.\nPassword length should be between 8 and 128 characters ") 
 
